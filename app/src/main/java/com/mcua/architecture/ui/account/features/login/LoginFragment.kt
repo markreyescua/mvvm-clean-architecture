bb0d520232610af09a5c566e1e8c6ecb8a56aa82
Mark Cua <mark.r.cua@gmail.com>
2021-06-07 06:44:39 +0800

    feat: add fragment data binding

--- app/src/main/java/com/mcua/architecture/ui/account/features/login/LoginFragment.kt
+++ app/src/main/java/com/mcua/architecture/ui/account/features/login/LoginFragment.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mcua.architecture.R
 import com.mcua.architecture.base.BaseFragment
+import com.mcua.architecture.databinding.FragmentLoginBinding
+import com.mcua.architecture.util.AppLog
 
 class LoginFragment : BaseFragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
-    
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnClickMe.setOnClickListener {
+                AppLog.e("button clicked")
+            }
+        }
+    }
+
 }
