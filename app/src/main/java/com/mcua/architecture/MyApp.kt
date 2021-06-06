9fb260ea02dd7bfdf0aa3ad135adbaa54d51be82
Mark Cua <mark.r.cua@gmail.com>
2021-06-07 06:16:05 +0800

    feat: add gradle flavors configurations

--- app/src/main/java/com/mcua/architecture/MyApp.kt
+++ app/src/main/java/com/mcua/architecture/MyApp.kt
@@ -2,12 +2,15 @@ package com.mcua.architecture
 
 import android.app.Application
 import com.mcua.architecture.util.AppLog
+import com.mcua.architecture.util.Constants.Companion.IS_PRODUCTION
 
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppLog.plant(AppLog.DebugTree())
+        if (!IS_PRODUCTION) {
+            AppLog.plant(AppLog.DebugTree())
+        }
     }
 
 }
