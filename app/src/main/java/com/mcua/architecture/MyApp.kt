b415e4fbc406a6c377a62a348bbea9e5ceed9a48
Mark Cua <mark.r.cua@gmail.com>
2021-06-07 06:26:37 +0800

    feat: add multidex support

--- app/src/main/java/com/mcua/architecture/MyApp.kt
+++ app/src/main/java/com/mcua/architecture/MyApp.kt
@@ -1,6 +1,8 @@
 package com.mcua.architecture
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.mcua.architecture.util.AppLog
 import com.mcua.architecture.util.Constants.Companion.IS_PRODUCTION
 
@@ -11,6 +13,11 @@ class MyApp : Application() {
         if (!IS_PRODUCTION) {
             AppLog.plant(AppLog.DebugTree())
         }
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
 }
