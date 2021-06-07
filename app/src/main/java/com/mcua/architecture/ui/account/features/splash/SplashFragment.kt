da428a3a7ef0f559aadbdd229ae0d13e5cc7bac6
Mark Cua <mark.r.cua@gmail.com>
2021-06-07 08:45:30 +0800

    feat: add splash screen

--- app/src/main/java/com/mcua/architecture/ui/account/features/splash/SplashFragment.kt
+++ app/src/main/java/com/mcua/architecture/ui/account/features/splash/SplashFragment.kt
@@ -4,16 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mcua.architecture.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import com.mcua.architecture.base.BaseFragment
+import com.mcua.architecture.databinding.FragmentSplashBinding
 
 class SplashFragment : BaseFragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: SplashViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.startSplashScreenTimeout()
+        viewModel.timeout.observe(viewLifecycleOwner, Observer { timeout ->
+            if (timeout) {
+                goToLogin()
+            }
+        })
+    }
+
+    private fun goToLogin() {
+        val action = SplashFragmentDirections.actionGoToLogin()
+        Navigation.findNavController(binding.root).navigate(action)
     }
 
 }
---
+++ app/src/main/java/com/mcua/architecture/ui/account/features/splash/SplashViewModel.kt
@@ -0,0 +1,20 @@
+package com.mcua.architecture.ui.account.features.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashViewModel : ViewModel() {
+
+    private val _timeout = MutableLiveData<Boolean>()
+    val timeout: LiveData<Boolean> = _timeout
+
+    fun startSplashScreenTimeout() = viewModelScope.launch {
+        delay(3000)
+        _timeout.postValue(true)
+    }
+
+}
