7491c622b92a22b7d53d6306437b6ac1ad64abe7
Mark Cua <mark.r.cua@gmail.com>
2021-06-13 21:18:02 +0800

    feat: add extensions

--- app/src/main/java/com/mcua/architecture/core/util/ext/AppExtension.kt
+++ app/src/main/java/com/mcua/architecture/core/util/ext/AppExtension.kt
@@ -1,5 +1,32 @@
 package com.mcua.architecture.core.util.ext
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import com.mcua.architecture.core.util.network.NetworkUtil
+import timber.log.Timber
 
 fun isNetworkAvailable() = NetworkUtil.isNetworkAvailable()
+
+fun safeLogDebug(msg: String) = Timber.d(msg)
+
+fun safeLogError(msg: String) = Timber.e(msg)
+
+fun safeLogInfo(msg: String) = Timber.i(msg)
+
+fun safeLogVerbose(msg: String) = Timber.v(msg)
+
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Activity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
